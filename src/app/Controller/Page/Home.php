a631a4eb29221c898c4f4a51f25b88e8b74a93af
Wilfried <64591090+la2spaille@users.noreply.github.com>
2023-02-06 09:28:44 +0100

    Update Home.php

--- src/app/Controller/Page/Home.php
+++ src/app/Controller/Page/Home.php
@@ -22,7 +22,7 @@ class Home extends Controller
 
         // SEO
         $this->head['title'] = 'Error — 404 Not Found';
-        $this->head['description'] = "concours #4 : Créer et coder une page 404";
+        $this->head['description'] = "concours #3 : Créer et coder une page 404";
         $this->head['opengraph'] = '/og/index.png';
 
         // Robots
