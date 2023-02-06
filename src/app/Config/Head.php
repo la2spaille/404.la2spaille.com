ce63df173beea3607e3d5bee7eec29d5a1f685be
Wilfried <la2spaille@gmail.com>
2023-02-06 09:19:16 +0100

    Update Head.php

--- src/app/Config/Head.php
+++ src/app/Config/Head.php
@@ -5,8 +5,8 @@ namespace App\Config;
 class Head {
 
     public static function data () {
-        $head['urlBase'] = 'https://www.404.la2spaille.studio'; // Desktop version only with protocol
-        $head['serverName'] = 'www.404.la2spaille.studio'; // Desktop or mobile without protocol
+        $head['urlBase'] = 'https://www.404-la2spaille.myhostme.space'; // Desktop version only with protocol
+        $head['serverName'] = 'www.404-la2spaille.myhostme.space'; // Desktop or mobile without protocol
 
         $head['twitter']['pseudo']  = '@la2spaille';
         $head['twitter']['creator'] = '@la2spaille';
